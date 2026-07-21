30a72da465ae1fd8e35d37ca18b499a6ec61e961
nashera28bomu-pixel <nashera28bomu@gmail.com>
2026-07-21 07:58:37 +0300

    Delete server/src directory

--- server/src/routes/search.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { globalSearch } = require('../controllers/search.controller');
-
-const router = express.Router();
-
-// GET /api/search?q=query
-router.get('/', globalSearch);
-
-module.exports = router;
